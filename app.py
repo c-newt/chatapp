6e7587df5d05a26859929a35cfbfeded7cfa8ce9
joe-debugg <77209152+joe-debugg@users.noreply.github.com>
2021-03-21 14:15:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,10 @@ def index():
 def handleMessage(msg):
     print('Message: ' + msg)
     send(msg, broadcast=True)
-    
-if __name__ == '__main__': # __name__!
-    socketio.run(
-        app,
-        host=os.getenv('IP', '127.0.0.1'),
-        port=int(os.getenv('PORT', 5000)),
-        debug=True
-    )
+   
+socketio.run(
+    app,
+    host=os.getenv('IP', '127.0.0.1'),
+    port=int(os.getenv('PORT', 5000)),
+    debug=True
+)
